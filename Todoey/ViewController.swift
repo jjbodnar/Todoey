25b7bf336ec36140611b4feba5028bd5bbfb2e10
James Bodnar <jjbod22@gmail.com>
2019-08-09 13:07:52 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by James Bodnar on 8/9/19.
+//  Copyright © 2019 James Bodnar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
