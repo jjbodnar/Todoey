977160a21530b66c466ad457c5a31eff84b16f42
James Bodnar <jjbod22@gmail.com>
2019-09-03 09:31:32 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by James Bodnar on 8/30/19.
+//  Copyright © 2019 James Bodnar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
